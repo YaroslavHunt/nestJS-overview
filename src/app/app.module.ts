1b29721723a5805a3500e28f7eac6f5a2e3155a2
YaroslavHunt <yaroslavohotskiy@gamil.com>
2024-12-28 00:34:39 +0200

    Налаштування Sequelize, .env, підключення до postgres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from '../config';
+import {SequelizeModule} from '@nestjs/sequelize';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [config],
+  }),
+  SequelizeModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      dialect: 'postgres',
+      host: configService.get('db_host'),
+      port: configService.get('db_port'),
+      username: configService.get('db_user'),
+      password: configService.get('db_password'),
+      database: configService.get('db_name'),
+      synchronize: true,
+      autoLoadModels: true,
+      models: []
+    })
   })],
   controllers: [AppController],
   providers: [AppService],
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,8 @@
-import * as process from 'node:process';
-
 export default () => ({
-  port: process.env.PORT
+  port: process.env.PORT,
+  db_host: process.env.DB_HOST,
+  db_port: process.env.PGSQL_PORT,
+  db_user: process.env.PGSQL_USER,
+  db_password: process.env.PGSQL_PASSWORD,
+  db_name: process.env.PGSQL_NAME
 })
