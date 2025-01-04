174989c27e1fbae5479f86e15ae053bee95537da
YaroslavHunt <yaroslavohotskiy@gamil.com>
2025-01-04 15:37:42 +0200

    Додавання типізації. Валідація по email(unique). Кастомізація помилок.

--- src/user/users.service.ts
+++ src/user/users.service.ts
@@ -1,25 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from './models/user.model';
 import { InjectModel } from '@nestjs/sequelize';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto';
+import { AppError } from '../common/errors';
 
 @Injectable()
 export class UsersService {
 
-    constructor(@InjectModel(User) private readonly userRepository: typeof User) {}
+  constructor(@InjectModel(User) private readonly userRepository: typeof User) {
+  }
 
-    async hashPassword(password: string) {
-        return bcrypt.hash(password, 10);
-    }
+  async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
 
-    getUsers() {
-        return null;
-    }
+  async findUserByEmail(email: string) {
+    return this.userRepository.findOne({where:{email}})
+  }
 
-    async createUser(dto):Promise<CreateUserDto> {
-        dto.password = await this.hashPassword(dto.password);
-        await this.userRepository.create(dto);
-        return dto;
-    }
+  getUsers() {
+    return null;
+  }
+
+  async createUser(dto: CreateUserDto): Promise<CreateUserDto> {
+    const existUser = await this.findUserByEmail(dto.email);
+    if(existUser) throw new BadRequestException(AppError.USER_EXIST);
+
+    dto.password = await this.hashPassword(dto.password);
+    await this.userRepository.create({
+      username: dto.username,
+      password: dto.password,
+      email: dto.email,
+    });
+    return dto;
+  }
 }
